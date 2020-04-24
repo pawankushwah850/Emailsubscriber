f8795db870a88b726893013ac898b85743626934
pawan kushwah <50065404+pawankushwah850@users.noreply.github.com>
2020-04-24 21:48:46 +0530

    Update settings.py

--- subscribe/settings.py
+++ subscribe/settings.py
@@ -87,8 +87,8 @@ EMAIL_BACKEND ='django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'python3community@gmail.com'
-EMAIL_HOST_PASSWORD = 'P@wan7869'
+EMAIL_HOST_USER = 'p*********3community@gmail.com'
+EMAIL_HOST_PASSWORD = '********'
 """########################################################################################"""
 
 
