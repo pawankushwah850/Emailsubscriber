615ac615b226e98abf527ecd88fe8d18103cd75d
pawan kushwah <50065404+pawankushwah850@users.noreply.github.com>
2020-04-24 21:30:25 +0530

    Add files via upload

---
+++ subscribe/__init__.py
@@ -0,0 +1,1 @@
+"""some data will here"""
---
+++ subscribe/urls.py
@@ -0,0 +1,26 @@
+"""emailSubscribe URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path,include
+
+admin.site.site_header="Python 3 community"
+admin.site.site_title="p3c admin-panal"
+admin.site.index_title="Wel-come Ad.pawan kushwah"
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('',include('subscribers.urls')),
+]
